155ae927945fe9b71784116c90f6e17185091607
John Huffman <johnhuffman@Johns-MacBook-Pro.local>
2016-08-16 16:49:26 -0600

    updates to remove players from screen on disconnect

--- public/javascripts/indexDriver.js
+++ public/javascripts/indexDriver.js
@@ -14,7 +14,7 @@ manager = objectManager()
 /*
 * Other players live in here
 */
-people = {}
+people = []
 /*
 * Store the chickens locally
 */
@@ -43,38 +43,47 @@ socket.on('connected', function(data) {
         if(other.id == player.id) {
             continue;
         }
-        people[other.id] = new playerClass(other.x, other.y, other.id)
+        people.push(new playerClass(other.x, other.y, other.id))
     }
 })
 /*
-* When register other players when someone else joins
+* Register other players when someone else joins
 */
-socket.on('connections', function(data) {
-    var others = data.people
+socket.on('new_connection', function(data) {
+    var other = data.person
     /*
-    * Init the other players in the game
+    * Init the new players in the game
     */
-    for(other in others) {
-        var other = others[other]
-        /*
-        * Skip self in list to prevent duplicates
-        */
-        if(other.id == player.id) {
-            continue;
-        }
-        people[other.id] = new playerClass(other.x, other.y, other.id)
-    }
+    people.push(new playerClass(other.x, other.y, other.id))
 })
 socket.on('update_position', function(data) {
-    people[data.id].update_position({
-        x : data.x,
-        y : data.y
-    })
+    /*
+    * Find the player to move and update their position on screen
+    */
+    for(person in people) {
+        if(people[person].id == data.id) {
+            people[person].update_position({
+                x : data.x,
+                y : data.y
+            })
+            break;
+        }
+    }
 })
-socket.on('disconnect', function(data) {
-    for(player in people) {
-        if(player.id == data.id) {
-            people.splice(player, 1);
+socket.on('disconnected', function(data) {
+    /*
+    * Find player who disconnected
+    */
+    for(person in people) {
+        if(people[person].id == data.id) {
+            /*
+            * Remove player from screen
+            */
+            $('#'+people[person].id).remove()
+            /*
+            * Remove player from game
+            */
+            people.splice(person, 1)
             break;
         }
     }
