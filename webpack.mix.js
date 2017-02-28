316dd9afcd61734bcc90f01711dbe16b72cd661e
John Huffman <johnhuffman@Johns-Air.hsd1.co.comcast.net>
2017-02-28 08:21:29 -0700

    updates to packages, gitignore, added react assets

---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+/*
+mix.js('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css');
+*/
+mix.combine([
+   'node_modules/react/dist/react.min.js',
+   'node_modules/react-dom/dist/react-dom.min.js',
+   'node_modules/babel-standalone/babel.min.js'
+], 'public/javascripts/react-assets.js');
